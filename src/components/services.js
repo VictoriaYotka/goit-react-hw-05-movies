b9161801bcc28f968718c5eb65fca45488113d03
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-06-03 17:07:24 +0300

    add fetch on user query

--- src/components/services.js
+++ src/components/services.js
@@ -1,16 +1,16 @@
 // https://api.themoviedb.org/3/movie/550?api_key=1eb19aee2b556e872aeb4d12f3e3d0ef
-const BASE_URL = 'https://api.themoviedb.org/3'
-const KEY = '1eb19aee2b556e872aeb4d12f3e3d0ef';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const OPTIONS = {headers: {Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxZWIxOWFlZTJiNTU2ZTg3MmFlYjRkMTJmM2UzZDBlZiIsInN1YiI6IjY0NmY2NjQzMTdjNDQzMDBjMjI0OTMzZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nSQ1KLIUK97f2ypbV38GEeFR4HBt9RE_gR1cSa6SUjM'}}
+// const KEY = '1eb19aee2b556e872aeb4d12f3e3d0ef';
 
-async function fetchFunc (query) {
-    const response = await fetch(`${BASE_URL}/${query}?api_key=${KEY}`);
-
+function fetchFunc (query) {
+    return fetch(`${BASE_URL}/${query}`, OPTIONS).then( response => {
         if(!response.ok) {
             return Promise.reject(response.status)
         }
-
-        const data = await response.json();
-        return data;    
+        return response.json(); 
+    })
 }
 
 export default fetchFunc;
+
--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,6 +1,47 @@
+import { useState, useEffect } from "react";
+import fetchFunc from "components/services";
+import { NavLink } from "react-router-dom";
+
 const Movies = () => {
+  const [query, setQuery] = useState('');
+  // const [isLoading, setIsLoading] = useState(false);
+  const [films, setFilms] = useState([]);
+  
+useEffect(() => {
+  if(query !== '') {
+    // setIsLoading(true);
+    fetchFunc(`search/movie?query=${query}`)
+    .then(({results}) => {
+      setFilms([...results])
+      if(results.length === 0) alert("We've found nothing. Try another query!")})
+    .catch(console.log)
+    // .finally(setIsLoading(false))
+  }}, [query])
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const query = event.target.elements.film.value.trim();
+  
+    if(query === '') return
+  
+    setQuery(query);
+    event.target.reset();
+  }
+
   return (
-    <div>Movies</div>
+    <>
+    <form onSubmit={handleSubmit}>
+      <input name="film" type="text" autoFocus />
+      <button type="submit">Search</button>
+    </form>
+
+    {/* {isLoading && <p>Loading...</p>} */}
+
+    {films && <ul>
+      {films.map(({id, title}) => <li key={id}><NavLink>{title}</NavLink></li>)}
+      </ul>}
+
+    </>
   )
 }
 
