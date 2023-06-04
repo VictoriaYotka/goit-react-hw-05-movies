5b3e41a7fd69a8e0177875464264db86e4203859
VictoriaYotka <115709692+VictoriaYotka@users.noreply.github.com>
2023-06-04 17:09:58 +0000

    Deploying to gh-pages from @ VictoriaYotka/goit-react-hw-05-movies@b885b89bc9800c769ad0d56f7085fa34a0841fe6 🚀

--- static/js/871.6c224ee6.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[871],{871:function(e,i,t){t.r(i);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"Reviews"})}}}]);
-//# sourceMappingURL=871.6c224ee6.chunk.js.map
