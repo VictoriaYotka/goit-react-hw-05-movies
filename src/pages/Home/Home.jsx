5ecc577daecaba81eb12df40270a732f1049c8f8
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-06-05 12:09:57 +0300

    add style details

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
     setIsLoading(true);
     fetchFunc('trending/all/day')
     .then(({results}) => {
-      console.log(results);
+      // console.log(results);
       setTrends([...results])})
     .catch(console.log)
     .finally(() => setIsLoading(false));
@@ -22,7 +22,7 @@ const Home = () => {
     <div>
       <h2 className={css.title}>Popular today:</h2>
       {isLoading && <p>Loading...</p>}
-      <ul>
+      <ul className={css.list}>
         {trends.map(({id, title, name}) => <li key={id} className='list_item'><NavLink to={`/movies/${id}`} state={{ from: location }} className='link'>{title ?? name}</NavLink></li>)}
       </ul>
     </div>
